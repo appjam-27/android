a830636273003861654f6756369b8a069eec5663
JangWoojun <woojun0107@naver.com>
2024-08-18 06:08:23 +0900

    feat: SplashActivity 구현

--- app/src/main/java/com/seogaemo/candu/SplashActivity.kt
+++ app/src/main/java/com/seogaemo/candu/SplashActivity.kt
@@ -1,20 +1,17 @@
 package com.seogaemo.candu
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 2000)
     }
 }
