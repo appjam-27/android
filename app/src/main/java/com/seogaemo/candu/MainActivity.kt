af072a15fac338dea28adc0fc808f946429ee90e
JangWoojun <woojun0107@naver.com>
2024-08-17 21:19:24 +0900

    feat: systemBars 설정

--- app/src/main/java/com/seogaemo/candu/MainActivity.kt
+++ app/src/main/java/com/seogaemo/candu/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.seogaemo.candu
 
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.seogaemo.candu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupSystemBarsInsets()
+
+
+    }
+
+    private fun setupSystemBarsInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
